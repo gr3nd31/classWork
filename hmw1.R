d03148591c7f5c39cce82592b02bfa609e2ae4a2
Raas <jneedham@uab.edu>
2019-05-27 11:05:24 -0500

    Added comments to the original code

--- hmw1.R
+++ hmw1.R
@@ -1,7 +1,9 @@
+#Load the appropriate libraries
 library(purrr)
 library(ggplot2)
 library(cowplot)
 
+#This is the graphing function
 gguc <- function(x=1,y=1){
   ggplot(data = iris, aes_string(x = iris[[x]], y = iris[[y]], color = iris$Species))+
     geom_point()+
@@ -17,12 +19,16 @@ gguc <- function(x=1,y=1){
           legend.text = element_text(size = 18))
 }
 
+#This creates names from the original dataframe that can be used to assign unique varible names
+#If you want to use different variables, you can alter the index to set the names
 dat1 <- names(iris)[1:4]
 dat1 <- set_names(dat1)
 
+#This iterates the graphing function throughout the dataframe based on the set names
 all_plots2 = map(dat1,
                  ~map(dat1, gguc, y = .x))
 
+#This arranges the graph and adds the labels
 labs <- c("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P")
 thing <- plot_grid(all_plots2[[1]][[1]]+theme(legend.position = "none"),
                    all_plots2[[1]][[2]]+theme(legend.position = "none"),
@@ -43,9 +49,11 @@ thing <- plot_grid(all_plots2[[1]][[1]]+theme(legend.position = "none"),
                    nrow = 4, ncol = 4,
                    labels = labs)
 
+#This adds the common legend to the bottom of the graph
 legnd <- get_legend(all_plots2[[1]][[1]])
 things <- plot_grid(thing, legnd, ncol = 1, rel_heights = c(1, 0.05))
 
+#This saves the image as a 'svg'
 svg("hmw1.svg", height = 11, width = 8.5)
 print(things)
 dev.off()
